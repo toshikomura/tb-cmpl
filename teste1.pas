828bd04e0fecbc49a44ff786777bd6f2f2cd1897
Gustavo Toshi Komura <gtk10@c3sl.ufpr.br>
2013-05-25 17:13:24 -0300

    Update test to check if the repetition is work
    Signed-off-by: Gustavo Toshi Komura <gtk10@c3sl.ufpr.br>

--- teste1.pas
+++ teste1.pas
@@ -1,12 +1,14 @@
 program exemplo75 (input, output);
 var m,n :  integer;
 begin
-  a := 2;
   n := 1;
   m := n;
   m := 1 + 1;
   m := 2 * m;
   m := n / 2;
-  a := m - n;
   n := ( 1 + m ) * 3;
+  while ( n ) do
+  begin
+    n := m;
+  end;
 end.
