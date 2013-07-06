38ca466e1475b749e54df503c6a5bfe1f6c1149d
Gustavo Toshi Komura <gtk10@c3sl.ufpr.br>
2013-07-05 23:20:37 -0300

    Include token function as "funcao"
    Signed-off-by: Gustavo Toshi Komura <gtk10@c3sl.ufpr.br>

--- compilador.h
+++ compilador.h
@@ -24,7 +24,7 @@ typedef enum simbolos {
   simb_soma, simb_subtracao, simb_multiplicacao, simb_divisao,
   simb_igual, simb_diferente, simb_maior, simb_menor, simb_nao, simb_e, simb_ou,
   simb_enquanto, simb_para, simb_faca, simb_repita, simb_ate, simb_se, simb_entao, simb_senao,
-  simb_procedimento
+  simb_procedimento, simb_funcao
 } simbolos;
 
 
