76e8a61706f3ff6f01998c349fa0162aa83a5a93
Gustavo Toshi Komura <gtk10@c3sl.ufpr.br>
2013-07-10 18:05:32 -0300

    Include types and diference between value and reference
    Include new function "insere_tipo_parametro_Simbolo_TB_SIMB"
    that insert how many parameters, type of the parameters and
    passenger as value or reference.
    Update function "imprime_Simbolo_TB_SIMB" to print all the
    parameters, type and passenger.
    
    Signed-off-by: Gustavo Toshi Komura <gtk10@c3sl.ufpr.br>

--- compiladorF.c
+++ compiladorF.c
@@ -40,7 +40,7 @@ void inicia_variaveis_globais () {
     dados = malloc ( sizeof (char)*TAM_TOKEN);
     categoria = malloc ( sizeof (char)*TAM_TOKEN);
     tipo = malloc ( sizeof (char)*TAM_TOKEN);
-    tipo_parametro = malloc ( sizeof (char)*TAM_TOKEN);
+    tipo_valor_referencia = malloc ( sizeof (char)*TAM_TOKEN);
     tipo_retorno = malloc ( sizeof (char)*TAM_TOKEN);
     nome_proc_func = malloc ( sizeof (char)*TAM_TOKEN);
 
@@ -81,40 +81,45 @@ void imprime_Simbolo_TB_SIMB () {
 
     printf ( "|| simb | tipo | categ | rotu | tip_param | tip_retor | niv lex | desloc | qtd_param\n");
 
+    tipos_parametros_l *tip_para_aux;
     no_tabela_simbolos_p *slot_tb_simb_aux;
 
     slot_tb_simb_aux = p_tb_simb->primeiro;
     while ( slot_tb_simb_aux != NULL) {
-        printf("|| %s | %s | %s | %s | %s | %s | %d | %d | %d ||\n", slot_tb_simb_aux->simbolo, slot_tb_simb_aux->tipo, slot_tb_simb_aux->categoria, slot_tb_simb_aux->rotulo, slot_tb_simb_aux->tipo_parametro, slot_tb_simb_aux->tipo_retorno, slot_tb_simb_aux->nivel_lexico, slot_tb_simb_aux->desloc, slot_tb_simb_aux->qtd_parametros);
+        printf ( "|| %s | %s | %s | %s | %s | %d | %d | %d ", slot_tb_simb_aux->simbolo, slot_tb_simb_aux->tipo, slot_tb_simb_aux->categoria, slot_tb_simb_aux->rotulo, slot_tb_simb_aux->tipo_retorno, slot_tb_simb_aux->nivel_lexico, slot_tb_simb_aux->desloc, slot_tb_simb_aux->qtd_parametros);
+
+        tip_para_aux = slot_tb_simb_aux->primeiro_tipo_parametro;
+
+        while ( tip_para_aux != NULL) {
+            printf ( "|- %s | %s ", tip_para_aux->valor_referencia, tip_para_aux->tipo);
+            tip_para_aux = tip_para_aux->prox;
+        }
+
+        printf ( "||\n");
+
         slot_tb_simb_aux = slot_tb_simb_aux->prox;
     }
 
 }
 
 /* Função que insere um simbolo na tabela de simbolos */
-void empilha_Simbolo_TB_SIMB ( char *simb, char *ca, char *rot, char *ti_para, char *ti_ret, int nivel_l, int des, int qt_para) {
+void empilha_Simbolo_TB_SIMB ( char *simb, char *ca, char *rot, int nivel_l, int des) {
 
     char *simbol = malloc ( sizeof ( char)*TAM_TOKEN);
     char *cate = malloc ( sizeof ( char)*TAM_TOKEN);
-    char *tip_param = malloc ( sizeof ( char)*TAM_TOKEN);
-    char *tip_retor = malloc ( sizeof ( char)*TAM_TOKEN);
 
     strcpy( simbol, simb);
     strcpy( cate, ca);
-    strcpy( tip_param, ti_para);
-    strcpy( tip_retor, ti_ret);
 
     no_tabela_simbolos_p *novo_slot_tb_simb = malloc( sizeof ( no_tabela_simbolos_p ));
 
     novo_slot_tb_simb->simbolo = simbol;
     novo_slot_tb_simb->categoria = cate;
     novo_slot_tb_simb->rotulo = rot;
-    novo_slot_tb_simb->tipo_parametro = tip_param;
-    novo_slot_tb_simb->tipo_retorno = tip_retor;
 
     novo_slot_tb_simb->nivel_lexico = nivel_l;
     novo_slot_tb_simb->desloc = des;
-    novo_slot_tb_simb->qtd_parametros = qt_para;
+    novo_slot_tb_simb->qtd_parametros = 0;
 
     novo_slot_tb_simb->prox = p_tb_simb->primeiro;
 
@@ -138,6 +143,56 @@ void insere_tipo_Simbolo_TB_SIMB ( char *ti, int qtd_vars) {
     for ( i = 0; i < qtd_vars; i++) {
         slot_tb_simb_aux->tipo = tip;
         slot_tb_simb_aux = slot_tb_simb_aux->prox;
+    }
+
+    imprime_Simbolo_TB_SIMB ();
+
+}
+
+/* Função que insere tipo de parametro da função ou procedimento na tabela de simbolos */
+/* Além disso a função insere a quantidade de parametros */
+void insere_tipo_parametro_Simbolo_TB_SIMB ( char *simb, char *va_re, char *ti, int qtd_param) {
+
+    char *tip = malloc ( sizeof ( char)*TAM_TOKEN);
+    char *var_refer = malloc ( sizeof ( char)*TAM_TOKEN);
+    int i, j;
+    tipos_parametros_l *tip_para_aux;
+    no_tabela_simbolos_p *slot_tb_simb_aux;
+
+    strcpy ( tip, ti);
+    strcpy ( var_refer, va_re);
+
+    for ( j = 0; j < qtd_param; j++) {
+
+        tipos_parametros_l *tip_para = malloc ( sizeof ( tipos_parametros_l ));
+        slot_tb_simb_aux = p_tb_simb->primeiro;
+
+        tip_para->valor_referencia = var_refer;
+        tip_para->tipo = tip;
+        tip_para->prox = NULL;
+
+        while ( slot_tb_simb_aux != NULL && strcmp( slot_tb_simb_aux->simbolo, simb)) {
+            slot_tb_simb_aux->tipo = tip;
+            slot_tb_simb_aux = slot_tb_simb_aux->prox;
+        }
+
+        if ( slot_tb_simb_aux != NULL) {
+
+            if ( slot_tb_simb_aux->qtd_parametros == 0) {
+                slot_tb_simb_aux->primeiro_tipo_parametro = tip_para;
+                slot_tb_simb_aux->qtd_parametros = 1;
+            }
+            else {
+
+                tip_para_aux = slot_tb_simb_aux->primeiro_tipo_parametro;
+
+                for ( i = 1; i < slot_tb_simb_aux->qtd_parametros; i++)
+                    tip_para_aux = tip_para_aux->prox;
+
+                tip_para_aux->prox = tip_para;
+                slot_tb_simb_aux->qtd_parametros++;
+            }
+        }
     }
 
     imprime_Simbolo_TB_SIMB ();
